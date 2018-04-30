410202f8b62e084e30224c7ea75692856ed25cf8
Tyler Smith <tyler@technologytherapy.com>
2018-04-30 11:42:47 -0400

    Added Schedules

--- MBTA.php
+++ MBTA.php
@@ -1,5 +1,8 @@
 <?php
   class MBTA {
+    public static $trips = [];
+    public static $stops = [];
+    
     public static function requestRoutes(){
       $curl = curl_init('https://api-v3.mbta.com/routes');
       curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
@@ -15,6 +18,59 @@
         array_push($routesByType[$route->attributes->description], $route);
       }
       return $routesByType;
+    }
+
+    public static function getScheduleById($id) {
+      $curl = curl_init('https://api-v3.mbta.com/schedules?filter%5Broute%5D='.$id);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+      $result = curl_exec($curl);
+      curl_close($curl);
+      return json_decode($result);
+    }
+
+    public static function getScheduleByIdGroupedByTrip($id){
+      $schedule = self::getScheduleById($id);
+      if (empty($schedule->data)) {
+        //should really return the same error here
+        return false;
+      }
+      $trips = [];
+      foreach($schedule->data as $stop){
+        $tripid = $stop->relationships->trip->data->id;
+        $trips[$tripid] = $trips[$tripid] ?? [];
+        array_push($trips[$tripid], $stop);
+      }
+      return $trips;
+    }
+    public static function getTrip($id){
+      if (!empty(self::$trips[$id])) {
+        return self::$trips[$id];
+      }
+      $curl = curl_init('https://api-v3.mbta.com/trips/'.$id);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+      $result = curl_exec($curl);
+      curl_close($curl);
+      self::$trips[$id] = json_decode($result);
+      return self::$trips[$id];
+    }
+
+    public static function getStop($id){
+      if (!empty(self::$stops[$id])) {
+        return self::$stops[$id];
+      }
+      $curl = curl_init('https://api-v3.mbta.com/stops/'.$id);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+      $result = curl_exec($curl);
+      curl_close($curl);
+      self::$stops[$id] = json_decode($result);
+      return self::$stops[$id];
+    }
+    public static function getStopName($id){
+      var_dump(self::getStop($id));
+      return self::getStop($id)->data->attributes->name;
+    }
+    public static function getTripName($id){
+      return self::getTrip($id)->data->attributes->name;
     }
   }
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
  include './MBTA.php';
  $routesTypes = MBTA::routesByType();
- 
- ?>  
+ $routeId = !empty($_GET['route']) ? $_GET['route'] : false;
 
- <table>
-  <?php foreach ($routesTypes as $route_name => $routes): ?>
-      <th><?php echo $route_name; ?></th>
-      <?php foreach ($routes as $route): ?>
-        <tr style="background-color: <?php echo $route->attributes->color; ?>">
-          <td><?php echo $route->attributes->long_name !== '' ? $route->attributes->long_name  : $route->attributes->short_name; ?></td>
-        </tr>
+ if (!empty($routeId)):
+  $schedule = MBTA::getScheduleByIdGroupedByTrip($routeId); 
+    foreach($schedule as $tripId => $stops):
+    ?>
+    <h3><?php echo $tripId; ?></h3>
+      <?php foreach($stops as $stop): ?>
+      <?php echo $stop->relationships->stop->data->id; ?>
+      <?php $date = new DateTime($stop->attributes->arrival_time); echo $date->format('H:i:s'); ?>
+      <br/>
       <?php endforeach; ?>
-  <?php endforeach; ?>
-</table>
+    <?php endforeach;?>
+  <?php else: ?>
+    <table>
+      <?php foreach ($routesTypes as $route_name => $routes): ?>
+          <th><?php echo $route_name; ?></th>
+          <?php foreach ($routes as $route): ?>
+            <tr style="background-color: <?php echo $route->attributes->color; ?>">
+              <td><a style="color: black;" href="/?route=<?php echo $route->id; ?>"><?php echo $route->attributes->long_name !== '' ? $route->attributes->long_name  : $route->attributes->short_name; ?></a></td>
+            </tr>
+          <?php endforeach; ?>
+      <?php endforeach; ?>
+    </table>
+  <?php endif; ?>
